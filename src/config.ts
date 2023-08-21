513234496360d0f5ce89550c1c9053b36635a02d
Zach Robinson <zachary.robinson@interstates.com>
2023-08-21 10:32:38 -0500

    Cast the env variable input as the branded type.

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import { Angle, ArenaId, SharkId } from "./api/gameplay";
 
 export const baseUrl = process.env.HOST || "http://127.0.0.1:3000";
-export const arenaId = process.env.GAME_ID || ("0000-004J" as ArenaId);
-export const sharkId =
-  process.env.PLAYER_ID || ("0bf0fda7-53Sb4-42c4-ad77-6e82e0fc4513" as SharkId);
+export const arenaId = (process.env.GAME_ID || "0000-004J") as ArenaId;
+export const sharkId = (process.env.PLAYER_ID ||
+  "0bf0fda7-53Sb4-42c4-ad77-6e82e0fc4513") as SharkId;
 
 export const sharkSettings = {
   forwardSpeed: 6,
